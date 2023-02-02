d98974ce0a0d172ce6843121ac21b6831ae15576
hai161102 <ripallrank@gmail.com>
2023-02-02 13:26:24 +0700

    init

---
+++ app/src/main/java/com/example/game2dlib/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.game2dlib;
+
+import com.example.game2dlib.databinding.ActivityMainBinding;
+import com.haiprj.base.view.BaseActivity;
+
+public class MainActivity extends BaseActivity<ActivityMainBinding> {
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void addEvent() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+}
